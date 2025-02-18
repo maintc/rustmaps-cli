ddf8a6257244bae3e39ab2c3b5d53c40fb72f44a
heavyscientist <github@mainloot.com>
2025-02-17 16:45:37 -0800

    added test when reading dir for config

--- pkg/rustmaps/config_test.go
+++ pkg/rustmaps/config_test.go
@@ -32,6 +32,16 @@ func TestGenerator_LoadConfig(t *testing.T) {
 			}),
 			wantErr: true,
 		},
+		{
+			name: "Test LoadConfig fail tmp",
+			generator: NewMockedGenerator(t, &Generator{
+				configPath: "/tmp/",
+				config:     types.Config{},
+				maps:       []*types.Map{},
+				rmcli:      &api.RustMapsClient{},
+			}),
+			wantErr: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
